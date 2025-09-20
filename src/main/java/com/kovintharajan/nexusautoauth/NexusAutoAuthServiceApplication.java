524c23b146320f2e82eb406c2b95d7162ab0e1d8
Kovintharajan Krishnasuntharam <KovinthKrishna@gmail.com>
2025-09-20 23:04:13 +0530

    Initial commit

---
+++ src/main/java/com/kovintharajan/nexusautoauth/NexusAutoAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kovintharajan.nexusautoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAutoAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusAutoAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kovintharajan/nexusautoauth/NexusAutoAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovintharajan.nexusautoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusAutoAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
