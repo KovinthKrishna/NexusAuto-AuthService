a9226a156eef5234dddcfe84f82d41652a383657
Kovintharajan Krishnasuntharam <KovinthKrishna@gmail.com>
2025-09-27 23:45:41 +0530

    Implemented JwtService

---
+++ src/main/java/com/kovintharajan/nexusautoauth/dto/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.kovintharajan.nexusautoauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+}
---
+++ src/main/java/com/kovintharajan/nexusautoauth/service/JwtService.java
@@ -0,0 +1,38 @@
+package com.kovintharajan.nexusautoauth.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .claims().add(extraClaims).and()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 48))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
