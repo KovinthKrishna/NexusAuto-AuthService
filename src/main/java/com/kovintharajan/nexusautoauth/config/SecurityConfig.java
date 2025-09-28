9e0d421682bc56b63810f1da991cb44ecba7483d
Kovintharajan Krishnasuntharam <KovinthKrishna@gmail.com>
2025-09-28 19:49:15 +0530

    Create a JWT authentication filter

--- src/main/java/com/kovintharajan/nexusautoauth/config/SecurityConfig.java
+++ src/main/java/com/kovintharajan/nexusautoauth/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.kovintharajan.nexusautoauth.config;
 
+import com.kovintharajan.nexusautoauth.handler.CustomAuthEntryPoint;
 import com.kovintharajan.nexusautoauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -9,17 +10,20 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserRepository userRepository;
+    private final CustomAuthEntryPoint customAuthEntryPoint;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -40,13 +44,18 @@ public class SecurityConfig {
 
     // This bean configures application's security rules
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(customAuthEntryPoint))
+                // Set session management to stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Add the JWT filter before the standard username/password filter
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
